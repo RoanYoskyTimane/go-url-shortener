671af1ecb0e20890212fc14d5af0dcaf23576b28
Roan Yosky Timane <roanyosky@gmail.com>
2026-08-06 10:28:50 +0200

    Create database schema

---
+++ sql/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE urls (
+                      id            BIGSERIAL PRIMARY KEY,
+                      short_code    VARCHAR(16) NOT NULL UNIQUE,
+                      original_url  TEXT NOT NULL,
+                      access_count  BIGINT NOT NULL DEFAULT 0,
+                      created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                      updated_at    TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
