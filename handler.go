986e5a3d582007d6296a41cfc36a55f12e6cfe12
Roan Yosky Timane <roanyosky@gmail.com>
2026-08-06 17:03:23 +0200

    Create the update short url handler

--- handler.go
+++ handler.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"github/RoanYoskyTimane/go-url-shortener/db"
 	"log"
 	"net/http"
 	"net/url"
 	"time"
+
+	"github.com/go-chi/chi/v5"
 )
 
 // POST /api/v1/urls
@@ -67,7 +70,48 @@ func (u URLHandler) createShortUrl(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(resp)
 }
-func (u URLHandler) getShortUrl(w http.ResponseWriter, r *http.Request)    {}
+
+// GET /{shortCode}
+func (u URLHandler) getShortUrl(w http.ResponseWriter, r *http.Request) {
+	shortCode := chi.URLParam(r, "shortCode")
+	if shortCode == "" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"error":"Not found"}`))
+		return
+	}
+
+	ctx := r.Context()
+
+	// Check Redis cache
+	targetURL, err := u.RDB.Get(ctx, shortCode).Result()
+	if err == nil {
+		go func() {
+			_ = u.Queries.IncrementAccessCount(context.Background(), shortCode)
+		}()
+
+		http.Redirect(w, r, targetURL, http.StatusFound)
+		return
+	}
+
+	record, err := u.Queries.GetURLByShortCode(ctx, shortCode)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"error":"Not found"}`))
+		return
+	}
+
+	_ = u.RDB.Set(ctx, record.ShortCode, record.OriginalUrl, 24*time.Hour).Err()
+
+	// Increment access count in database
+	go func() {
+		_ = u.Queries.IncrementAccessCount(context.Background(), record.ShortCode)
+	}()
+
+	http.Redirect(w, r, record.OriginalUrl, http.StatusFound)
+}
+
 func (u URLHandler) updateShortUrl(w http.ResponseWriter, r *http.Request) {}
 func (u URLHandler) deleteShortUrl(w http.ResponseWriter, r *http.Request) {}
 func (u URLHandler) urlStatistics(w http.ResponseWriter, r *http.Request)  {}
